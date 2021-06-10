b4cfcf2b86f49a08fef01c30e55b28d7d1ec3436
yashshivhare911119@gmail.com <yashshivhare911119@gmail.com>
2021-06-10 15:10:45 +0530

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppcore_exercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
